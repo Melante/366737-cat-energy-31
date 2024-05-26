3e75a2455e800593b475ac6fe4903eea3c0b6aac
Melante <Melante@users.noreply.github.com>
2024-05-26 06:31:44 +0000

    Deploying to gh-pages from @ Melante/366737-cat-energy-31@c9a5bc68b6420b3e25fe786104fa4cc17ee8a8a6 🚀

---
+++ 11/scripts/index.js
@@ -0,0 +1,1 @@
+var n=document.querySelector(".main-nav__toggle"),e=document.querySelector(".main-nav__open ");n.onclick=function(){n.classList.toggle("main-nav__toggle--open"),e.classList.toggle("main-nav--open")};
