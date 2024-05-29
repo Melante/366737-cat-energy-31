945dee429662601fd0315fe68470314265a868a4
Mari <33049628+Melante@users.noreply.github.com>
2024-05-29 14:11:39 +0500

    Меню без js

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,6 +1,9 @@
 /* в этот файл добавляет скрипты*/
 const burger = document.querySelector('.navigation__toggle');
-const nav = document.querySelector('.navigation__list ');
+const nav = document.querySelector('.navigation__list');
+const block = document.querySelector('.navigation__list--bg');
+
+block.classList.remove('navigation__list--bg');
 
 burger.onclick = function() {
   burger.classList.toggle('navigation__toggle--open');
